144d75fb320afcc958db6e6f5050570e9699e2fe
Krish-Mahorkar <krishmahorkar20@gmail.com>
2025-05-12 15:32:46 +0530

    Initial Commit

---
+++ day42-git-check/src/main/java/com/capgemini/day42_git_check/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.day42_git_check;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
